d9729ef3b7aada46e223a386752f80b0beea9eaa
System Administrator <root@MacBook-Pro-de-Patrick.local>
2016-04-22 11:58:54 +0200

    Jeu de test pour tester le code  enter the commit message for your changes. Lines starting

--- src/test/java/ch/hesge/projetecole/test/AppTest.java
+++ src/test/java/ch/hesge/projetecole/test/AppTest.java
@@ -3,6 +3,7 @@ package ch.hesge.projetecole.test;
 import ch.hesge.projetecole.domaine.Article;
 import ch.hesge.projetecole.domaine.Commande;
 import org.testng.annotations.BeforeMethod;
+import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 import java.util.*;
 import java.awt.List;
@@ -17,19 +18,54 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    private ArrayList maListe= new ArrayList();
-    
- 
+    private ArrayList<Article> maListe= new ArrayList();
+    private ArrayList<Article> maListeTotal=new ArrayList();
+    private ArrayList<Commande> maListeCommande=new ArrayList();
+    private ArrayList<Commande> maListeCommandeToutes=new ArrayList();
+    private int nbr;
+    private int nbrP=0;
+    private double sommeDeux;
     
     @Test
-    public  void addArticle()
+    public  void addArticleTest()
     {
+        int tailleDebut = maListe.size();
         Article at = new Article(2,"Lunettes virtuels",23.55);
         maListe.add(at);
-        for (int i = 0; i < maListe.size(); i++) {
-            System.out.println(maListe.toString());
-        }
+        int tailleFinal = maListe.size();
+
+        assertTrue(tailleFinal == tailleDebut + 1);
     }
     
    
+
+   @Test
+   public void TestNbrArticleParCommandeEtSonMontantTotal()
+   {
+       Article arti = new Article(25,"Lunettes de soleil",67.00);
+        Article att=new Article(3,"Casque Virtuel",45.55);
+        Article art=new Article(5,"Camera de recul",56.00);
+        Article at = new Article(2,"Lunettes virtuels",23.55);
+        maListe.add(att);
+        maListe.add(art);
+        maListe.add(arti);
+        maListe.add(at);
+        Commande com = new Commande(at,1);
+        Commande comm = new Commande(att,1);
+        Commande comma = new Commande(art,1);
+        Commande commande = new Commande(arti,1);
+        maListeCommande.add(com);
+        maListeCommande.add(comm);
+        maListeCommande.add(comma);
+        maListeCommande.add(commande);
+        for (int i = 0; i < maListeCommande.size(); i++) {
+           nbr+=((Commande)maListeCommande.get(i)).getQte();
+        }
+       for (int i = 0; i < maListeCommande.size(); i++) {
+           nbrP=(maListeCommande.get(i)).getQte();
+           sommeDeux+=maListeCommande.get(i).getArticle().getPrix()*nbrP;
+       }
+       System.out.println(nbr+" Articles ");
+       System.out.println(sommeDeux+" CHF pour totalité de la commande");
+   }
 }
