673f38017972dc234959b5128d66a3d3e4fde56e
System Administrator <root@MacBook-Pro-de-Patrick.local>
2016-05-06 10:26:43 +0200

    Commit modification de la classe Main et ajout dependance Mockito

--- src/main/java/ch/hesge/projetecole/Main.java
+++ src/main/java/ch/hesge/projetecole/Main.java
@@ -5,6 +5,8 @@
  */
 package ch.hesge.projetecole;
 
+import java.io.FileNotFoundException;
+
 /**
  *
  * @author patrickribeiroamaral
@@ -13,10 +15,14 @@ public class Main {
     private static Mesarticles mesArticles;
     
     public static void main(String[] args) {
+        try{
         System.getProperty("Articles.txt");
         String fileName = "Articles.txt";
         System.out.println("Traitement des articles dans \"" + fileName + "\" :");
         mesArticles = new Mesarticles();
         mesArticles.traiterFichier(fileName);
     } 
+        catch (Error e0){e0.getSuppressed();
+            
+        }}
 }
